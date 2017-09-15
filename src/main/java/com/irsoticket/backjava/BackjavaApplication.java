0cf7a45fcad2ce0cb95c49644a87211a27e7a152
Kaltentod <emanuelgomez91@gmail.com>
2017-09-15 20:09:00 -0300

    Primer commit

---
+++ src/main/java/com/irsoticket/backjava/BackjavaApplication.java
@@ -0,0 +1,12 @@
+package com.irsoticket.backjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackjavaApplication.class, args);
+	}
+}
