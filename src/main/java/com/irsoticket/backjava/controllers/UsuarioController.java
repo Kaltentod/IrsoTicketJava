81460ea59914719666ec3dec47939ecdade49786
Kaltentod <emanuelgomez91@gmail.com>
2017-09-28 20:57:02 -0300

    ABM USR

--- src/main/java/com/irsoticket/backjava/controllers/UsuarioController.java
+++ src/main/java/com/irsoticket/backjava/controllers/UsuarioController.java
@@ -1,16 +1,17 @@
 package com.irsoticket.backjava.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.irsoticket.backjava.models.Usuario;
 import com.irsoticket.backjava.repositories.UsuarioRepository;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
 
 @RestController
 public class UsuarioController {
@@ -20,13 +21,35 @@ public class UsuarioController {
 	
 	//Trae todos los usuarios
 	@GetMapping("/findAll")
-	public Object test(){
+	public Object findAll(){
 		return usuarioRepository.findAll();
 	} 
 	
-	//inserta un nuevo usuario
+	//Inserta un nuevo usuario
 	@PostMapping("/altaUsuario")
-	public void altaUsuario(@RequestBody Usuario usuario){
-		usuarioRepository.save(usuario);
+	public Object altaUsuario(@RequestBody Usuario usuario){
+		return usuarioRepository.save(usuario);
+	}
+	
+	//Modificar nuevo usuario
+	@PutMapping("/modificarUsuario")
+	public Object modificarUsuario(@RequestBody Usuario usuario){
+		
+		Usuario usuarioMod = usuarioRepository.findOne(usuario.getId());
+		
+		usuarioMod.setApellido(usuario.getApellido());
+		usuarioMod.setNombre(usuario.getNombre());
+		usuarioMod.setPass(usuario.getPass());
+		usuarioMod.setRol(usuario.getRol());
+		
+		return usuarioRepository.save(usuarioMod);
+	}
+	
+	//Borrar un usuario
+	@DeleteMapping("/borrarUsuario/{id}")
+	public Object borrarUsuario(@PathVariable long id)throws MessagingException {
+		Usuario usuario = usuarioRepository.findOne(id);
+		usuarioRepository.delete(usuario);
+		return usuario;
 	}
 }
