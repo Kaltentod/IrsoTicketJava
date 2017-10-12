04e7d537654e5f11a790713def5468d1cf2beb28
Kaltentod <emanuelgomez91@gmail.com>
2017-10-11 21:27:17 -0300

    Heroku

--- src/main/java/com/irsoticket/backjava/controllers/UsuarioController.java
+++ src/main/java/com/irsoticket/backjava/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.irsoticket.backjava.controllers;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,34 +9,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.irsoticket.backjava.models.Usuario;
 import com.irsoticket.backjava.repositories.UsuarioRepository;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
 
 @RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
 	//Trae todos los usuarios
-	@GetMapping("/findAll")
+	@GetMapping
 	public Object findAll(){
 		return usuarioRepository.findAll();
 	} 
 	
 	//Inserta un nuevo usuario
-	@PostMapping("/altaUsuario")
-	public Object altaUsuario(@RequestBody Usuario usuario){
+	@PostMapping
+	public Usuario altaUsuario(@RequestBody Usuario usuario){
 		return usuarioRepository.save(usuario);
 	}
 	
-	//Modificar nuevo usuario
-	@PutMapping("/modificarUsuario")
-	public Object modificarUsuario(@RequestBody Usuario usuario){
+	//Modificar un usuario
+	@PutMapping("/{id}")
+	public Usuario modificarUsuario(@PathVariable long id,@RequestBody Usuario usuario){
 		
-		Usuario usuarioMod = usuarioRepository.findOne(usuario.getId());
+		Usuario usuarioMod = usuarioRepository.findOne(id);
 		
 		usuarioMod.setApellido(usuario.getApellido());
 		usuarioMod.setNombre(usuario.getNombre());
@@ -45,8 +50,8 @@ public class UsuarioController {
 	}
 	
 	//Borrar un usuario
-	@DeleteMapping("/borrarUsuario/{id}")
-	public Object borrarUsuario(@PathVariable long id) {
+	@DeleteMapping("/{id}")
+	public Usuario borrarUsuario(@PathVariable long id) {
 		Usuario usuario = usuarioRepository.findOne(id);
 		usuarioRepository.delete(usuario);
 		return usuario;
