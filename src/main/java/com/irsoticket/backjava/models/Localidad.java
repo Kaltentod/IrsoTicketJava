a57c819d6e0b4f55c187d0a7dec55892e0516799
Kaltentod <emanuelgomez91@gmail.com>
2017-11-03 21:00:54 -0300

    Api

--- src/main/java/com/irsoticket/backjava/models/Localidad.java
+++ src/main/java/com/irsoticket/backjava/models/Localidad.java
@@ -6,17 +6,17 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name="localidades")
+@JsonIgnoreProperties(value={"hibernateLazyInitializer", "handler"})
 public class Localidad {
 	
 	//Atributos
--- src/main/java/com/irsoticket/backjava/models/Usuario.java
+++ src/main/java/com/irsoticket/backjava/models/Usuario.java
@@ -11,7 +11,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import org.hibernate.validator.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
+@JsonIgnoreProperties(value={"hibernateLazyInitializer", "handler"})
 @Table(name="usuarios")
 public class Usuario {
 	
